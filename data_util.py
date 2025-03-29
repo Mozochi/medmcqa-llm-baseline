06a66a9bd5def2992744b2035f4f910b797b3807
Mozochi <50766817+Mozochi@users.noreply.github.com>
2025-03-29 14:05:12 +0000

    Update data_util.py

--- data_util.py
+++ data_util.py
@@ -3,7 +3,17 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 import argparse
+import os
 
+def check_datasets_file():
+    try:
+        os.mkdir('datasets')
+    except FileExistsError:
+        pass
+    except PermissionError:
+        print("Permission denied: Unable to create datasets.")
+    except Exception as e:
+        print(f"An error has occurred: {e}")
 
 def json_to_dataframe(dir):
     data = [] # List to append all JSON objects to
@@ -46,13 +56,20 @@ def load_and_transform_json(dir, **kwargs): # dir: directory of json data, optio
 
 
 def df_to_parquet(df_data, file_name):
+    check_datasets_file()
     parquet_table = pa.Table.from_pandas(df_data)
-    pq.write_table(parquet_table, f'{file_name}.parquet')
+    pq.write_table(parquet_table, f'datasets/{file_name}.parquet')
+
+def transform_dataset_to_parquet(dir, **kwargs):
+    new_data = load_and_transform_json(dir, **kwargs)
+    file_name = os.path.splitext(os.path.basename(dir))[0]
+    df_to_parquet(new_data, file_name)
 
 
 # Only used for debugging
 def _df_to_csv(df_data, file_name): 
-    csv_data = df_data.to_csv(f'{file_name}.csv', sep=',')
+    check_datasets_file()
+    csv_data = df_data.to_csv(f'datasets/{file_name}.csv', sep=',')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Data Utilities')
